bb133b50d2ebedd4fdda5731b269aca6e3fb3e6c
Barney Hanlon <barney@shrikeh.net>
2024-12-06 12:23:43 +0000

    Add contract for MessageRequestFactory

---
+++ src/Http/Action/MessageRequestFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shrikeh\ApiContext\Http\Action;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Shrikeh\App\Message;
+
+
+interface MessageRequestFactory
+{
+    public function createMessageFromRequest(ServerRequestInterface $request): Message;
+}
