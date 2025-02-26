5d11e03ee0886af557ae7f7f422de25ba34cbfeb
Barney Hanlon <barney@shrikeh.net>
2025-02-26 21:47:04 +0000

    Tweak to get to the Kernel from tests

--- src/Context/Console.php
+++ src/Context/Console.php
@@ -9,22 +9,30 @@ use Shrikeh\ApiContext\Context;
 use Shrikeh\ApiContext\ServiceProvider\Console as ConsoleServiceProvider;
 use Shrikeh\ApiContext\Kernel\Service;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 final readonly class Console implements Context
 {
-    public function __construct(private string $rootDir)
-    {
+    public function __construct(
+        private string $rootDir,
+        private Container $container = new Container()
+    ) {
+
     }
 
     public function __invoke(array $context): mixed
     {
-        $container = new Container();
-        $container->register(new ConsoleServiceProvider(
+        return new Application($this->kernel($context));
+    }
+
+    public function kernel(array $context): KernelInterface
+    {
+        $this->container->register(new ConsoleServiceProvider(
             $this->rootDir,
             $context['APP_ENV'],
             (bool) $context['APP_DEBUG']
         ));
 
-        return new Application($container[Service::KERNEL->key()]);
+        return $this->container[Service::KERNEL->key()];
     }
 }
